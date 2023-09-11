664b30d02162e4eeb65865eab22334b20c5249ce
ezeflt <feltenezechiel1@gmail.com>
2023-09-11 23:09:15 +0200

    backend

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -2,22 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app/app.module");
-const path_1 = require("path");
+const common_1 = require("@nestjs/common");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.useStaticAssets((0, path_1.join)(__dirname, '..', 'public'));
-    app.setBaseViewsDir((0, path_1.join)(__dirname, '..', 'views'));
-    app.setViewEngine('hbs');
-    app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-        next();
-    });
-    app.enableCors({
-        allowedHeaders: "*",
-        origin: "*"
-    });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
+    app.enableCors();
+    app.useGlobalPipes(new common_1.ValidationPipe());
     await app.listen(3000);
 }
 bootstrap();
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,30 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { ConfigService } from '@nestjs/config';
-import { NestExpressApplication } from "@nestjs/platform-express";
-import { join } from "path";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-      AppModule,
-  );
 
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
-
-  app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-      next();
-  });
-
-  app.enableCors({
-      allowedHeaders:"*",
-      origin: "*"
-  });
-
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
