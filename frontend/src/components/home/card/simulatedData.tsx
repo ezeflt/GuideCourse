9c42079431cb7f4a4c932db9a7277844cdf3cac9
ezeflt <feltenezechiel1@gmail.com>
2023-09-11 19:42:06 +0200

    final push

--- frontend/src/components/home/card/simulatedData.tsx
+++ frontend/src/components/home/card/simulatedData.tsx
@@ -2,8 +2,8 @@
 
 // the card datas
 const cardData : Array<any> = [
-    { titleCard : 'guides', titleBtn : 'see guides', img : './logoHomePage/guide.svg', resume : "Enim virtute ut virtute cultuque officiorumque eo delectari iucundius amare animante ut Nihil ut Nihil admodum iucundius enim amare animante ut Nihil ut Nihil admodum iucundius enim ",  },
-    { titleCard : 'courses', titleBtn : 'see courses', img : './logoHomePage/course.svg', resume : "Enim virtute ut virtute cultuque officiorumque eo delectari iucundius amare animante ut Nihil ut Nihil admodum iucundius enim amare animante ut Nihil ut Nihil admodum iucundius enim ",  }
+    { titleCard : 'guides', titleBtn : 'see guides', img : './logoHomePage/guide.svg', resume : "Access online guides, where you have the option to download them and view associated courses, each of which will enable you to follow a detailed tutorial. ",  },
+    { titleCard : 'courses', titleBtn : 'see courses', img : './logoHomePage/course.svg', resume : "Access our list of training courses, fully available. You have the opportunity to subscribe, and by doing so, you will reserve your spot in the course. ",  }
 ];
 
 export default cardData;
---
+++ readme.md
@@ -0,0 +1,52 @@
+<div id="" align="center">
+  <img src="./frontend/public/readmePresentation.png" alt="Morning logo" width="100%" />
+</div>
+
+# Présentation :
+Guides et Cours en ligne est une applications web qui permet de de visiter des guides (tuto) pour se reperer et par la suite vous avez la possibiltié de reserver des formations.
+
+## lien du site : https://fr
+
+# Technologie utilisée
+
+Frontend :
+- React (framework)
+- Typescript (langage)
+- gsap (librarie)
+- css (langage)
+- antd (librarie)
+- redux (librarie)
+
+Backend :
+- Node js (plateforme logicielle)
+- Typescript (langage)
+- Nest js (framework)
+- Class-transformer (librarie)
+- Class-validator (librarie)
+- Uid2 (librarie)
+- Bcrypt (librarie)
+
+## lancer l'applications en local 
+
+### Prérequis : 
+
+- Node js
+- Nest js
+
+Tapez ces lignes de commande dans votre terminal :
+
+
+`````
+cd frontend
+yarn install
+yarn start
+`````
+
+
+# Développeur 
+
+Ezechiel Felten </br>
+
+feltenezechiel1@gmail.com</br>
+[linkedin](https://www.linkedin.com/in/ezechiel-felten-475693248/)</br>
+[portfolio](https://ezechiel-felten.vercel.app/)</br>
