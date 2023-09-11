bc4edd34580eea072503ab11ef57c36a709b8367
ezeflt <feltenezechiel1@gmail.com>
2023-09-11 15:40:50 +0200

    final push

---
+++ frontend/src/components/home/card/simulatedData.tsx
@@ -0,0 +1,9 @@
+
+
+// the card datas
+const cardData : Array<any> = [
+    { titleCard : 'guides', titleBtn : 'see guides', img : './logoHomePage/guide.svg', resume : "Enim virtute ut virtute cultuque officiorumque eo delectari iucundius amare animante ut Nihil ut Nihil admodum iucundius enim amare animante ut Nihil ut Nihil admodum iucundius enim ",  },
+    { titleCard : 'courses', titleBtn : 'see courses', img : './logoHomePage/course.svg', resume : "Enim virtute ut virtute cultuque officiorumque eo delectari iucundius amare animante ut Nihil ut Nihil admodum iucundius enim amare animante ut Nihil ut Nihil admodum iucundius enim ",  }
+];
+
+export default cardData;
