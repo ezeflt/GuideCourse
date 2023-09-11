0c17df6dfc620e12a9f441aa56616b3bbcd3166c
ezeflt <feltenezechiel1@gmail.com>
2023-09-11 22:13:35 +0200

    backend

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -4,14 +4,15 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app/app.module");
 const common_1 = require("@nestjs/common");
 const config_1 = require("@nestjs/config");
+const platform_express_1 = require("@nestjs/platform-express");
+const cors = require("cors");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter());
     const configService = app.get(config_1.ConfigService);
-    app.enableCors({
-        origin: ['https://guide-course-frontend.vercel.app'],
-        methods: ['GET', 'POST'],
+    app.use(cors({
+        origin: 'https://guide-course-frontend.vercel.app',
         credentials: true,
-    });
+    }));
     app.useGlobalPipes(new common_1.ValidationPipe());
     await app.listen(process.env.PORT || 3000);
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,16 +2,17 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { ConfigService } from '@nestjs/config';
+import { ExpressAdapter } from "@nestjs/platform-express";
+import * as cors from 'cors'; // Importez le module cors
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, new ExpressAdapter());
   const configService = app.get(ConfigService);
-  app.enableCors({
-    origin: ['https://guide-course-frontend.vercel.app'],
-    methods: ['GET', 'POST'],
+  app.use(cors({
+    origin: 'https://guide-course-frontend.vercel.app',
     credentials: true,
-  });
+  }));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ||3000);
 }
