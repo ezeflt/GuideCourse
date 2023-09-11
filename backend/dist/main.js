4ce0dd6842df3ec615f1d4b825323926c969486d
ezeflt <feltenezechiel1@gmail.com>
2023-09-11 22:24:53 +0200

    backend

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -3,16 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app/app.module");
 const common_1 = require("@nestjs/common");
-const config_1 = require("@nestjs/config");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
-    const configService = app.get(config_1.ConfigService);
-    app.enableCors({
-        origin: ['https://guide-course-frontend.vercel.app'],
-        methods: ['GET', 'POST'],
-    });
+    app.enableCors();
     app.useGlobalPipes(new common_1.ValidationPipe());
-    await app.listen(process.env.PORT || 3000);
+    await app.listen(3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,12 +6,8 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule, { cors: true });
-  const configService = app.get(ConfigService);
-  app.enableCors({
-    origin: ['https://guide-course-frontend.vercel.app'],
-    methods: ['GET', 'POST'],
-  });
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ||3000);
+  await app.listen(3000);
 }
 bootstrap();
