edf908b96584670b391666b70cf4d80644a6ce44
ezeflt <feltenezechiel1@gmail.com>
2023-09-11 21:46:25 +0200

    backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.enableCors({
-    origin: '*'
+    origin: ['https://guide-course-frontend.vercel.app/']
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ||3000);
