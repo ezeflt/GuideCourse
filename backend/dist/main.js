a2b63c6da0bef2f7822edd9d8a4cd2f4aff04d95
ezeflt <feltenezechiel1@gmail.com>
2023-09-11 21:19:21 +0200

    backend

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -3,12 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app/app.module");
 const common_1 = require("@nestjs/common");
-const config_1 = require("@nestjs/config");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    const configService = app.get(config_1.ConfigService);
     app.enableCors({
-        origin: configService.get('https://guide-course-frontend.vercel.app/')
+        origin: '*'
     });
     app.useGlobalPipes(new common_1.ValidationPipe());
     await app.listen(process.env.PORT || 3000);
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 import { ValidationPipe } from "@nestjs/common";
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
   app.enableCors({
-    origin: configService.get('https://guide-course-frontend.vercel.app/')
+    origin: '*'
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ||3000);
