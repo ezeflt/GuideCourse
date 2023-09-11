1d82e9d73139d873e1f22e9d4bc2b9ad9d2b0fe6
ezeflt <feltenezechiel1@gmail.com>
2023-09-11 20:34:26 +0200

    final push

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://guide-course-frontend-mqmdtpper-ezeflt.vercel.app',
+    origin: 'https://guide-course-frontend.vercel.app/',
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ||3000);
