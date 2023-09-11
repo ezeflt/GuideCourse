0723f3342541c1db38cbfe457f7a1a14842abcfa
ezeflt <feltenezechiel1@gmail.com>
2023-09-11 20:44:21 +0200

    final push

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -6,7 +6,13 @@ const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: 'https://guide-course-frontend.vercel.app',
+        origin: [
+            'https://guide-course-frontend.vercel.app',
+            'http://localhost:3000',
+            'http://localhost:3001'
+        ],
+        methods: ["GET", "POST", "PUT"],
+        credentials: true,
     });
     app.useGlobalPipes(new common_1.ValidationPipe());
     await app.listen(process.env.PORT || 3000);
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,13 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://guide-course-frontend.vercel.app',
+    origin: [
+      'https://guide-course-frontend.vercel.app',
+      'http://localhost:3000',
+      'http://localhost:3001'
+    ],
+    methods: ["GET", "POST","PUT"],
+    credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ||3000);
